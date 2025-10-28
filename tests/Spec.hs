a671b4b378992a2fff3d35042d772471433702a8
Dwayne Crooks <me@dwaynecrooks.com>
2025-10-27 20:46:57 -0400

    Test Elm2Nix.Dependency

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -2,9 +2,11 @@ module Main (main) where
 
 import Test.Hspec
 
+import qualified Test.Elm2Nix.Dependency as Dependency
 import qualified Test.Elm2Nix.Version as Version
 
 
 main :: IO ()
-main =
+main = do
+    Dependency.main
     Version.main
---
+++ tests/Test/Elm2Nix/Dependency.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Elm2Nix.Dependency (main) where
+
+import Test.Hspec
+
+import Elm2Nix.Dependency (Dependency(..))
+import Elm2Nix.Version (Version(..))
+
+
+main :: IO ()
+main = hspec $
+    describe "Elm2Nix.Dependency" orderSpec
+
+
+orderSpec :: Spec
+orderSpec =
+    describe "order" $ do
+        it "elm-core-1.0.5 < elm-json-1.1.3" $
+            Dependency "elm" "core" (Version 1 0 5) < Dependency "elm" "json" (Version 1 1 3)
+
+        it "elm-browser-1.0.2 > elm-browser-1.0.1" $
+            Dependency "elm" "browser" (Version 1 0 2) > Dependency "elm" "browser" (Version 1 0 1)
