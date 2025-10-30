506c1d548b461bc85ebd5403d05c3e4a24638f7c
Dwayne Crooks <me@dwaynecrooks.com>
2025-10-30 05:26:23 -0400

    Show how to produce an elm.lock file

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
 module Main (main) where
 
+import qualified Data.Aeson as Json
 import qualified Elm2Nix.Data.FixedOutputDerivation as FOD
 
 
 main :: IO ()
 main =
-    FOD.fromFile "elm.json" >>= either print print
+    FOD.fromFile "elm.json" >>= either print (Json.encodeFile "elm.lock")
