4d8bc6cc8313cecfeb6e53f6777d2dcca69992c3
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-02 06:10:35 -0400

    Start to implement Elm2Nix.Data.Name

---
+++ src/Elm2Nix/Data/Name.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Elm2Nix.Data.Name
+    ( Author
+    , Name
+    , Package
+    , fromText
+    , toString
+    ) where
+
+import qualified Data.Text as Text
+
+import Data.Text (Text)
+
+
+data Name
+    = Name
+        { author :: Author
+        , package :: Package
+        }
+    deriving (Eq, Ord)
+
+
+instance Show Name where
+    show = toString "/"
+
+
+type Author = Text
+type Package = Text
+
+
+fromText :: Text -> Either String Name
+fromText t =
+    let
+        ( author, slashPackage ) =
+            Text.breakOn "/" t
+    in
+    case Text.uncons slashPackage of
+        Just ( '/', package ) ->
+            if Text.null author then
+                Left "author is empty"
+
+            else if Text.null package then
+                Left "package is empty"
+
+            else
+                Right $ Name author package
+
+        _ ->
+            Left "/ is missing"
+
+
+toString :: Text -> Name -> String
+toString separator (Name author package) =
+    Text.unpack $ author <> separator <> package
