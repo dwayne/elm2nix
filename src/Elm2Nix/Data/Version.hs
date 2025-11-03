c8bdbd7fbc1af1f0ba95196dea8e85e075422215
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-03 07:59:34 -0400

    Start to rethink Elm2Nix.Data.Version

--- src/Elm2Nix/Data/Version.hs
+++ src/Elm2Nix/Data/Version.hs
@@ -1,17 +1,20 @@
-module Elm2Nix.Data.Version (Version(..)) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.Aeson as Json
+module Elm2Nix.Data.Version (Version(..), fromText) where
+
+import qualified Data.Char as Char
 import qualified Data.Text as Text
 
-import Data.Aeson (ToJSON(..))
-import Numeric.Natural (Natural)
+import Control.Applicative (liftA3)
+import Data.Text (Text)
+import Data.Word (Word16)
 
 
 data Version
     = Version
-        { major :: Natural
-        , minor :: Natural
-        , patch :: Natural
+        { major :: {-# UNPACK #-} !Word16
+        , minor :: {-# UNPACK #-} !Word16
+        , patch :: {-# UNPACK #-} !Word16
         }
     deriving (Eq, Ord)
 
@@ -21,6 +24,64 @@ instance Show Version where
         show major ++ "." ++ show minor ++ "." ++ show patch
 
 
-instance ToJSON Version where
-    toJSON = Json.String . Text.pack . show
-    toEncoding = toEncoding . Text.pack . show
+fromText :: Text -> Maybe Version
+fromText t =
+    --
+    -- Expected format:
+    --
+    -- 1. Must be of the form MAJOR.MINOR.PATCH
+    -- 2. MAJOR, MINOR, and PATCH must each represent 16-bit unsigned integers: 0, 1, 2, ..., 65535
+    -- 3. Leading zeros are not allowed
+    --
+    case Text.splitOn "." t of
+        [ x, y, z ] ->
+            liftA3 Version (readWord16 x) (readWord16 y) (readWord16 z)
+
+        _ ->
+            Nothing
+
+
+readWord16 :: Text -> Maybe Word16
+readWord16 t =
+    if t == "0" then
+        Just 0
+
+    else
+        case Text.uncons t of
+            Just (  d, s ) | d /= '0' && Char.isDigit d ->
+                --
+                -- It starts with a non-zero digit
+                --
+                readWord16Helper (Char.digitToInt d) s
+
+            _ ->
+                --
+                -- It must be non-empty and the leading character must be a non-zero digit
+                --
+                Nothing
+
+
+readWord16Helper :: Int -> Text -> Maybe Word16
+readWord16Helper n t =
+    case Text.uncons t of
+        Just ( d, s ) ->
+            if Char.isDigit d then
+                let
+                    m = n * 10 + Char.digitToInt d
+                in
+                if m <= maxWord16 then
+                    readWord16Helper m s
+
+                else
+                    Nothing
+
+            else
+                Nothing
+
+        Nothing ->
+            Just $ fromIntegral n
+
+
+maxWord16 :: Int
+maxWord16 =
+    fromIntegral (maxBound :: Word16)
--- tests/Test/Elm2Nix/Data/Version.hs
+++ tests/Test/Elm2Nix/Data/Version.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Test.Elm2Nix.Data.Version (main) where
 
 import Test.Hspec
+
+import qualified Elm2Nix.Data.Version as Version
 
 import Elm2Nix.Data.Version (Version(..))
 
@@ -8,15 +12,25 @@ import Elm2Nix.Data.Version (Version(..))
 main :: IO ()
 main = hspec $
     describe "Elm2Nix.Data.Version" $ do
-        showSpec
+        fromTextSpec
         orderSpec
+        showSpec
 
 
-showSpec :: Spec
-showSpec =
-    describe "show" $
-        it "uses the major.minor.patch format" $
-            show (Version 1 2 3) == "1.2.3"
+fromTextSpec :: Spec
+fromTextSpec =
+    describe "fromText" $ do
+        it "must be of the X.Y.Z" $
+            Version.fromText "1.2" `shouldBe` Nothing
+
+        it "must not have a part with leading zeros" $
+            Version.fromText "1.02.3" `shouldBe` Nothing
+
+        it "must have each part be able to fit in a 16-bit unsigned integer" $
+            Version.fromText "1.2.65536" `shouldBe` Nothing
+
+        it "is valid when the input is of the form X.Y.Z and 0 <= X,Y,Z <= 65535" $
+            Version.fromText "0.1.65535" `shouldBe` Just (Version 0 1 65535)
 
 
 orderSpec :: Spec
@@ -27,3 +41,10 @@ orderSpec =
             -- N.B. "100.0.0" < "2.0.0"
             --
             Version 100 0 0 > Version 2 0 0
+
+
+showSpec :: Spec
+showSpec =
+    describe "show" $
+        it "uses the MAJOR.MINOR.PATCH format" $
+            show (Version 1 2 3) `shouldBe` "1.2.3"
