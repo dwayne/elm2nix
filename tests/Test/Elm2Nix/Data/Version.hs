7c8ca561d7210036f83965c272f8f51de10eabe9
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-05 04:18:43 -0400

    Refactor Test.Elm2Nix.Data.Version

--- tests/Test/Elm2Nix/Data/Version.hs
+++ tests/Test/Elm2Nix/Data/Version.hs
@@ -20,17 +20,19 @@ main = hspec $
 fromTextSpec :: Spec
 fromTextSpec =
     describe "fromText" $ do
-        it "must be of the X.Y.Z" $
-            Version.fromText "1.2" `shouldBe` Nothing
+        describe "valid input" $
+            it "example 1" $
+                Version.fromText "0.1.65535" `shouldBe` Just (Version 0 1 65535)
 
-        it "must not have a part with leading zeros" $
-            Version.fromText "1.02.3" `shouldBe` Nothing
+        describe "invalid input" $ do
+            it "must be of the form X.Y.Z" $
+                Version.fromText "1.2" `shouldBe` Nothing
 
-        it "must have each part be able to fit in a 16-bit unsigned integer" $
-            Version.fromText "1.2.65536" `shouldBe` Nothing
+            it "must not have a part with leading zeros" $
+                Version.fromText "1.02.3" `shouldBe` Nothing
 
-        it "is valid when the input is of the form X.Y.Z and 0 <= X,Y,Z <= 65535" $
-            Version.fromText "0.1.65535" `shouldBe` Just (Version 0 1 65535)
+            it "must have each part fit in a 16-bit unsigned integer" $
+                Version.fromText "1.2.65536" `shouldBe` Nothing
 
 
 orderSpec :: Spec
@@ -38,7 +40,7 @@ orderSpec =
     describe "order" $
         it "100.0.0 > 2.0.0" $
             --
-            -- N.B. "100.0.0" < "2.0.0"
+            -- N.B. As strings "100.0.0" < "2.0.0".
             --
             Version 100 0 0 > Version 2 0 0
 
@@ -46,5 +48,5 @@ orderSpec =
 showSpec :: Spec
 showSpec =
     describe "show" $
-        it "uses the MAJOR.MINOR.PATCH format" $
+        it "example 1" $
             show (Version 1 2 3) `shouldBe` "1.2.3"
