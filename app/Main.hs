cb049e2b2f4e448d5a0705416b1890062d4d8f0c
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-09 08:23:30 -0400

    Use Elm2Nix.writeElmLockFile in the app

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main (main) where
 
-import qualified Elm2Nix.ElmLock as ElmLock
+import qualified Elm2Nix
 
 
 main :: IO ()
 main =
-    ElmLock.generateLockFile "elm.json" "elm.lock" >>= either print (const $ putStrLn "Generated elm.lock!")
+    Elm2Nix.writeElmLockFile Elm2Nix.Expanded "test/data/elm.json" "elm.lock" >>= either print (const $ putStrLn "Generated elm.lock!")
