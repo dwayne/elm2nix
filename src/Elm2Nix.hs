23b7f37beaaffc6d61f44aa348c35d542fc17c7d
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-11 07:31:33 -0400

    Implement writeRegistryDatFile

--- src/Elm2Nix.hs
+++ src/Elm2Nix.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Elm2Nix (Format(..), WriteElmLockFileError(..), writeElmLockFile) where
+module Elm2Nix
+    ( Format(..), WriteElmLockFileError(..), writeElmLockFile
+    , WriteRegistryDatFileError, writeRegistryDatFile
+    ) where
 
 import qualified Data.Aeson as Json
 import qualified Data.Aeson.Encode.Pretty as Json
+import qualified Data.Binary as Binary
 import qualified Data.ByteString.Lazy as LBS
 
 import qualified Elm2Nix.Data.FixedOutputDerivation as FOD
+import qualified Elm2Nix.Data.RegistryDat as RegistryDat
 import qualified Elm2Nix.Lib.Json as Json
 
 import Elm2Nix.Data.FixedOutputDerivation (FixedOutputDerivation)
@@ -61,3 +66,17 @@ encodeExpanded output =
                 { Json.confCompare = Json.keyOrder [ "author", "package", "version", "sha256" ]
                 , Json.confTrailingNewline = True
                 }
+
+
+type WriteRegistryDatFileError = Json.DecodeFileError
+
+
+writeRegistryDatFile :: FilePath -> FilePath -> IO (Either WriteRegistryDatFileError ())
+writeRegistryDatFile input output = do
+    result <- Json.decodeFile input
+    case result of
+        Right elmJson ->
+            Right <$> Binary.encodeFile output (RegistryDat.fromElmJson elmJson)
+
+        Left err ->
+            return $ Left err
