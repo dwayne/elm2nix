ce0cb0d9114334808fb672eeb6fb88acec07888c
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-21 05:55:25 -0400

    Start using flake-utils
      - Use flake-utils.lib.eachDefaultSystem
      - Use flake-utils.lib.mkApp
      - Make elm2nix the default app
      - Restructure flake.nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,38 @@
 {
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
-      system = "x86_64-linux";
-
-      pkgs = nixpkgs.legacyPackages.${system};
-
       compiler = "ghc9102";
-      elm2nix = (pkgs.haskell.packages.${compiler}.callPackage ./elm2nix.nix {}).overrideAttrs (old: {
-        preCheck = (old.preCheck or "") + ''
-          export HSPEC_SKIP="(skip)"
-        '';
-      });
     in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "dev";
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-        packages = [
-          pkgs.haskell.compiler.${compiler}
-          pkgs.hlint
-          pkgs.cabal-install
-          pkgs.cabal2nix
-        ];
+        elm2nix = (pkgs.haskell.packages.${compiler}.callPackage ./elm2nix.nix {}).overrideAttrs (old: {
+          preCheck = (old.preCheck or "") + ''
+            export HSPEC_SKIP="(skip)"
+          '';
+        });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "dev";
 
-        shellHook = ''
-          export PROJECT_ROOT="$PWD"
-          export PS1="($name) $PS1"
-          export HSPEC_SKIP="(skip:network)"
-        '';
-      };
+          packages = [
+            pkgs.haskell.compiler.${compiler}
+            pkgs.hlint
+            pkgs.cabal-install
+            pkgs.cabal2nix
+          ];
 
-      packages.${system} = {
-        inherit elm2nix;
-        default = elm2nix;
-      };
-    };
+          shellHook = ''
+            export PROJECT_ROOT="$PWD"
+            export PS1="($name) $PS1"
+            export HSPEC_SKIP="(skip:network)"
+          '';
+        };
+
+        packages.default = elm2nix;
+        apps.default = flake-utils.lib.mkApp { drv = elm2nix; };
+      }
+    );
 }
