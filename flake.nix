253dbe1cca3d94ccc78ae422b6dc69256ad79fa0
Dwayne Crooks <me@dwaynecrooks.com>
2025-11-27 21:24:00 -0400

    Use the default ghc and haskellPackages

--- flake.nix
+++ flake.nix
@@ -2,14 +2,11 @@
   description = "elm2nix developer shell, package, and app";
 
   outputs = { self, nixpkgs, flake-utils }:
-    let
-      compiler = "ghc9102";
-    in
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        elm2nix = (pkgs.haskell.packages.${compiler}.callPackage ./elm2nix.nix {}).overrideAttrs (old: {
+        elm2nix = (pkgs.haskellPackages.callPackage ./elm2nix.nix {}).overrideAttrs (old: {
           preCheck = (old.preCheck or "") + ''
             export HSPEC_SKIP="(skip)"
           '';
@@ -20,7 +17,7 @@
           name = "dev";
 
           packages = [
-            pkgs.haskell.compiler.${compiler}
+            pkgs.ghc
             pkgs.hlint
             pkgs.cabal-install
             pkgs.cabal2nix
